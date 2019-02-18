7eec134ac029f8873a3ac66739e73f2948d853e3
sepiropht <sergembotta@mailoo.org>
2019-02-18 23:29:08 +0100

    impl scanner (wip)

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,21 +3,42 @@ use std::collections::HashMap;
 use Token;
 
 pub struct Scanner {
-    source: &str,
-    tokens: Vec<TokenType>
+    source: String,
+    tokens: Vec<TokenType>,
     start: u32,
     current: u32,
     line: u32
+    keywords: HashMap<&str, ToKenType>
 }
 
 impl Scanner {
-    pub fn new(source: &str) -> Scanner{
+    pub fn new(source: String) -> Scanner {
+        let keywords = HashMap::new();
+
+        keywords.insert("and" , TokenType::AND);
+        keywords.insert("class", TokenType::CLASS);
+        keywords.insert("else", TokenType::ELSE);
+        keywords.insert("false", TokenType::FALSE);
+        keywords.insert("for", TokenType::FOR);
+        keywords.insert("fun", TokenType::FUN);
+        keywords.insert("if", TokenType::IF);
+        keywords.insert("nil", TokenType::NIL);
+        keywords.insert("or", TokenType::OR);
+        keywords.insert("print", TokenType::PRINT);
+        keywords.insert("return", TokenType::RETURN);
+        keywords.insert("super",  TokenType::SUPER);
+        keywords.insert("this", TokenType::THIS);
+        keywords.insert("true",  TokenType::TRUE),
+        keywords.insert("var", TokenType::VAR);
+        keywords.insert("while", TokenType::WHILE);
+
         Scanner {
             source,
             tokens: vec![]
-            0,
-            0,
-            1
+            start: 0,
+            current: 0,
+            line 1,
+            keywords
         }
     }
 
@@ -26,14 +47,53 @@ impl Scanner {
             self.start = self.current;
             self.scan_token();
         }
-        tokens.push(Token::new(TokenType::EOF, "", null, self.line));
+        self.tokens.push(Token::new(TokenType::EOF, "", hashMap::new(), self.line));
         tokens
     }
 
-    fn scan_token(&self)
+    fn scan_token()  {
+        let c = self.advance();
+        match c {
+            Some('(') => add_token(TokenType::LEFT_PAREN),
+            Some(')') => add_token(TokenType::RIGHT_PAREN),
+            Some('{') => add_token(TokenType::LEFT_BRACE),
+            Some('}') => add_token(TokenType::RIGHT_BRACE),
+            Some(',') => add_token(TokenType::COMMA),
+            Some('.') => add_token(TokenType::DOT),
+            Some('-') => add_token(TokenType::MINUS),
+            Some('+') => add_token(TokenType::PLUS),
+            Some(';') => add_token(TokenType::SEMICOLON),
+            Some('*') => add_token(TokenType::STAR),
+            Some('!') => add_token(TokenType::BANG_EQUAL),
+            Some('=') => add_token(
+        }
+    }
 
     fn is_end(&self) -> bool {
         self.current >= self.source.len()
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        let res = self.source.get(self.current - 1)
+        self.current += 1;
+        res
+    }
+
+    fn _match (expected_char) -> bool {
+         if self.is_end() {
+             return false;
+         }
+         if self.source.get(self.current) !== Some(expected_char) {
+             return false;
+         }
+
+        self.current += 1;
+        return true;
+    }
+
+    fn add_token (&mut self, token: TokenType, text: Option<&str>) {
+        let text = &self.source.get(self.start..self.current).unwrap();
+        self.tokens.push(Token::new(token, text, HashMap::new(), self.line));
     }
 
 
